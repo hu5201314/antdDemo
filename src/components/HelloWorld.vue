debf915cae4abfb18b47d8ec1fb16eae5e2c28e8
huzhi <591205782@qq.com>
2019-03-05 21:58:25 +0800

    控件显示

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -5,7 +5,9 @@
       collapsible
       v-model="collapsed"
     >
-      <div class="logo" />
+      <div class="logo">
+        <a-icon type="strikethrough"  class="logoIcon"/>
+      </div>
       <a-menu theme="dark" mode="inline" :defaultSelectedKeys="['1']">
         <a-menu-item key="1">
           <a-icon type="user" />
@@ -18,6 +20,18 @@
         <a-menu-item key="3">
           <a-icon type="upload" />
           <span>nav 3</span>
+        </a-menu-item>
+         <a-menu-item key="4">
+          <a-icon type="form" />
+          <span>nav 4</span>
+        </a-menu-item>
+         <a-menu-item key="5">
+          <a-icon type="highlight" />
+          <span>nav 5</span>
+        </a-menu-item>
+        <a-menu-item key="6">
+          <a-icon type="edit" />
+          <span>nav 6</span>
         </a-menu-item>
       </a-menu>
     </a-layout-sider>
@@ -50,12 +64,17 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+#components-layout-demo-custom-trigger{
+  margin-top: -60px;
+  height: 630px;
+}
 #components-layout-demo-custom-trigger .trigger {
   font-size: 18px;
   line-height: 64px;
   padding: 0 24px;
   cursor: pointer;
   transition: color .3s;
+  float: left;
 }
 
 #components-layout-demo-custom-trigger .trigger:hover {
@@ -63,8 +82,13 @@ export default {
 }
 
 #components-layout-demo-custom-trigger .logo {
-  height: 32px;
-  background: rgba(255,255,255,.2);
+  height: 40px;
+  background: rgba(7, 11, 240, 0.5);
   margin: 16px;
+}
+#components-layout-demo-custom-trigger .logo .logoIcon{
+  font-size: 40px;
+  text-align: center;
+  color: #fff;
 }
 </style>
